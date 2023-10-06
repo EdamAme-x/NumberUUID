664eeff72ab6fe8efbd046473f1b6341c9e10eeb
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-06 14:06:22 +0900

    Update and rename index.js to index.ts

--- index.js
+++
@@ -1,3 +0,0 @@
-crypto.randomUUID().split("").filter(str => {
-    if (/[0-9]/.test(str)) return str;
-}).join("").concat(Math.random().toString().slice(2)).substring(0, 1)
---
+++ index.ts
@@ -0,0 +1,3 @@
+const numberUUID: number = parseInt(crypto.randomUUID().split("").filter(str => {
+    if (/[0-9]/.test(str)) return str;
+}).join("").concat(Math.random().toString().slice(2)).substring(0, 1))
